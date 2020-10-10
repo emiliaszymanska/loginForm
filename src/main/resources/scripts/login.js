e8ee0baf90aeda274fc50e0c55e2fbd7ea6f6350
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-10 18:53:22 +0200

    Add resources directory and test file

---
+++ src/main/resources/scripts/login.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#login-form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = `email=${this.email.value}&password=${this.password.value}`;
+    console.log(data);
+    login(data);
+});
+
+function login(data) {
+    fetch("http://localhost:8010/login",
+        {
+            // credentials: 'same-origin',
+            credentials: 'include',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+            if (!response.ok) { // OR !response.status
+                throw Error(response.statusText);
+            }
+            return response.text();
+        })
+        .then(function (message) { // user is authenticated (cookie send by server is set in browser)
+            console.log(message);
+        }).catch(function (error) { // user NOT authenticated (server returns different status than 200-299)
+        console.log(error);
+    });
+}
