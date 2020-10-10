1c39553146887b6e522e5c8f1aee06a9692d82ed
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-10 15:51:08 +0200

    Add Connector

---
+++ src/main/java/org/example/dao/Connector.java
@@ -0,0 +1,30 @@
+package org.example.dao;
+
+import org.example.helpers.JSONReader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Map;
+
+public class Connector {
+
+    private Map<String, String> dbData = JSONReader.read();
+    protected Connection connection;
+    protected Statement statement;
+
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(
+                    dbData.get("url"),
+                    dbData.get("user"),
+                    dbData.get("password")
+            );
+            statement = connection.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
