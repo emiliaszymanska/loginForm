263c38ff57ec254786195ed4f0ceb251ae3e38eb
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-10 16:10:27 +0200

    Add User class

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,28 @@
+package org.example.model;
+
+public class User {
+
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
