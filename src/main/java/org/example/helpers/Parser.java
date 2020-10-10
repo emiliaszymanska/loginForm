2a37534488a4865cc158fe76515d49e5c68ed576
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-10 17:05:25 +0200

    Add parser - helper

---
+++ src/main/java/org/example/helpers/Parser.java
@@ -0,0 +1,22 @@
+package org.example.helpers;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+
+    public static Map<String, String> parseData(String data) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = data.split("&");
+
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            // We have to decode the value because it's urlencoded => https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
+    }
+}
